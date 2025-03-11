5e7b53bc8d66282865ffa43eef71041f5527caa7
klipik_rez <78925089+klipikrez@users.noreply.github.com>
2025-03-11 16:16:37 +0100

    fix

--- Assets/Scripts/BootManager.cs
+++ Assets/Scripts/BootManager.cs
@@ -5,14 +5,14 @@ using UnityEngine;
 
 public class BootManager : MonoBehaviour
 {
-    public GameObject player;
+    public GameObject playerPrefab;
+    public PlayerMovement playerMovement;
 
 public void Awake()
 {
-    foreach (GameObject component in componentsToLoad)
-    {
-        Instantiate(component,Vector3.zero,quaternion.identity);
-    }
+
+        playerMovement = Instantiate(playerPrefab,Vector3.zero,quaternion.identity).GetComponent<PlayerMovement>();
+    
 }
 
 }
