4681d012008fb01f93e252ca32812dc1f3c3df8d
milospetrovic1618 <120582941+milospetrovic1618@users.noreply.github.com>
2025-03-12 07:49:07 +0100

    aa

--- Assets/Scripts/Login.cs
+++ Assets/Scripts/Login.cs
@@ -4,6 +4,7 @@ using TMPro;
 using UnityEditor;
 using UnityEngine;
 using UnityEngine.UI;
+using Unity.Mathematics;
 
 public class Login : MonoBehaviour
 {
@@ -52,14 +53,33 @@ public class Login : MonoBehaviour
         loginUI.SetActive(true);
         BootManager.Instance.uiOpen = true;
     }
-
+    public TMP_InputField usernameLogin;
+    public TMP_InputField passwordLogin;
+    public GameObject LogOut;
+    public GameObject logInButton;
     public void LogIn()
     {
-        string username = "";
-        string password = "";
+        string username = usernameLogin.text;
+        string password = passwordLogin.text;
         user = BootManager.Instance.data.LogIn(username,password);
 
+        if (user == null)
+        {
+            GameObject error = BootManager.Instance.Error;
+            Instantiate(error, Vector3.zero, quaternion.identity);
+        }
+        else
+        {
+            LogOut.SetActive(true);
+            loginUI.SetActive(false);
+            logInButton.SetActive(false);
+
+        }
         return;
+    }
+    public void Logout()
+    {
+
     }
 
     public void OpenFile()
